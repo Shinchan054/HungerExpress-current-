aa3109d9847c6aab4f665d0a224498c95d790dc1
Shinchan054 <108150140+Shinchan054@users.noreply.github.com>
2022-08-01 03:11:46 +0600

    homepages updated

--- routes/webpageRoutes/restaurant_home.js
+++ routes/webpageRoutes/restaurant_home.js
@@ -10,7 +10,10 @@ router.get('/:id',async function(req,res){
     var q="select name from restaurant where id = "+ids;
     const rest_name=await pool.query(q);
 
-    res.render('Webpages/restaurant_home',{id:ids,title:rest_name.rows[0].name});
+   var r = "select image_id from restaurant_image where restaurant_id = "+ids;
+    const rest_image=await pool.query(r);
+     let url = "/images/rest"+rest_image.rows[0].image_id+".png";
+    res.render('Webpages/restaurant_home',{id:ids,title:rest_name.rows[0].name , url:url});
 
 
 });
